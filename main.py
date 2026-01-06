3762a7724b6d271fa6acaa174d1a49a34e42936d
krylov16 <krylov-k@yandex.ru>
2026-01-06 15:50:08 +0300

    fa2

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ app = FastAPI()
 def read_root():
     html_content = "<h2>Hi, man!</h2>"
     return HTMLResponse(content=html_content)
+
+ 
+@app.get("/about")
+def read_root():
+    html_content = "<h2>My name is Konstantin</h2>"
+    return HTMLResponse(content=html_content)
